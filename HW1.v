9e8a096024fec847663dfb4fb1efbd77a254a8bd
C111112102 <c111112102@nkust.edu.tw>
2025-04-15 11:57:27 +0800

    Update HW1.v

--- HW1.v
+++ HW1.v
@@ -5,8 +5,4 @@ module switch_led_enable(
     input  wire [7:0] i_led_in,   // 來自 PS (AXI GPIO) 的 LED 資料
     output wire [7:0] o_led_out   // 輸出到 LED
     );
-    
-   // Switch 控制 LED 顯示反向輸出    
-    assign o_led_out = (i_sw[0] == 1'b1) ? ~i_led_in : i_led_in;
-
 endmodule
